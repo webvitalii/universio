c3f69528206b634ffb4a3256a7df73058e4d6db5
Vitalii <vitalii.skorokhod@gmail.com>
2016-06-22 16:40:43 -0500

    Updated the searchform.php

--- searchform.php
+++ searchform.php
@@ -1,8 +1,12 @@
 
 <form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
-	<input type="search" value="<?php echo esc_attr( get_search_query() ); ?>" class="fx-form-control" name="s" />
-	<button type="submit" class="fx-btn">
-		<i class="ionicon ion-ios-search-strong fx-icon fx-icon-20"></i> 
-		<?php echo esc_attr( __( 'Search', 'universio' ) ); ?>
-	</button>
+	<p>
+		<input type="search" value="<?php echo esc_attr( get_search_query() ); ?>" class="fx-form-control" name="s" />
+	</p>
+	<p>
+		<button type="submit" class="fx-btn">
+			<i class="ionicon ion-ios-search-strong fx-icon fx-icon-20"></i> 
+			<?php echo esc_attr( __( 'Search', 'universio' ) ); ?>
+		</button>
+	</p>
 </form>
